178c45a41ce153eedb455c7a497b93986beac5b0
Shashank Agarwal <shashankagarwal.official@gmail.com>
2024-03-06 17:41:01 +0530

    experience section added in the about page

--- src/app/(content)/about/page.tsx
+++ src/app/(content)/about/page.tsx
@@ -19,7 +19,7 @@ export default function About() {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center items-center sm:gap-y-4 md:gap-y-8 lg:gap-y-16 md:mx-10 lg:mx-40 my-20">
+      <div className="flex flex-col justify-center items-center sm:gap-y-2 md:gap-y-4 lg:gap-y-6 md:mx-10 lg:mx-40 my-20">
         <div className="flex flex-col gap-y-4">
           <h2 className="text-2xl text-center tracking-wide uppercase leading-6 lg:leading-12 lg:text-4xl bg-gradient-to-br from-slate-300 to-slate-500 inline-block text-transparent bg-clip-text">About me in</h2>
           <h2 className="text-4xl text-center tracking-wide uppercase leading-6 lg:leading-12 lg:text-6xl bg-gradient-to-br from-slate-300 to-slate-500 inline-block text-transparent rounded-md bg-clip-text p-4 border-2 border-slate-600">Bento Grid</h2>
@@ -29,9 +29,64 @@ export default function About() {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center items-center sm:gap-y-4 md:gap-y-8 lg:gap-y-16 md:mx-10 lg:mx-40 my-20">
+      <div className="py-6 mx-40">
+        <br />
+        <hr className="h-px bg-slate-200 border-0 dark:bg-slate-700" />
+        <br />
+      </div>
+
+      <div className="flex flex-col gap-y-12 mx-40">
+
+        <div className="flex flex-row justify-content items-center gap-x-4">
+          <div className="w-3 h-3 rounded-md bg-white shadow-lg shadow-white invert dark:invert-0"></div>
+          <h4 className="uppercase text-xl font-bold tracking-wide leading-6 lg:leading-12 lg:text-xl bg-gradient-to-br from-slate-300 to-slate-500 inline-block text-transparent bg-clip-text">Experience</h4>
+        </div>
+
+        <div className="grid grid-cols-2 gap-y-12">
+          <h4 className="text-2xl font-bold tracking-wide leading-6 lg:leading-12 lg:text-4xl bg-gradient-to-br from-slate-300 to-slate-500 inline-block text-transparent bg-clip-text">Orocorp</h4>
+          <div className="flex flex-col gap-y-4">
+            <div className="gap-y-2">
+              <h6 className="md:text-xl lg:text-2xl text-white font-regular tracking-wide ">Product Designer</h6>
+              <p className="text-xl text-slate-600"> 01/22 - 10/23</p>
+            </div>
+            <p className="text-xl text-slate-400 tracking-wide"> I've redefined the experience of Oro's core products.</p>
+          </div>
+
+          <h4 className="text-2xl font-bold tracking-wide leading-6 lg:leading-12 lg:text-4xl bg-gradient-to-br from-slate-300 to-slate-500 inline-block text-transparent bg-clip-text">Cogoport</h4>
+          <div className="flex flex-col gap-y-4">
+            <div className="gap-y-2">
+              <h6 className="md:text-xl lg:text-2xl text-white font-regular tracking-wide ">Product Designer</h6>
+              <p className="text-xl text-slate-600"> 10/21 - 01/22</p>
+            </div>
+            <p className="text-xl text-slate-400 tracking-wide"> I've designed the main lead management system for sales, marketing and finance people.</p>
+          </div>
+
+          <h4 className="text-2xl font-bold tracking-wide leading-6 lg:leading-12 lg:text-4xl bg-gradient-to-br from-slate-300 to-slate-500 inline-block text-transparent bg-clip-text">Facebook SOI India</h4>
+          <div className="flex flex-col gap-y-4">
+            <div className="gap-y-2">
+              <h6 className="md:text-xl lg:text-2xl text-white font-regular tracking-wide ">AR Designer & Developer</h6>
+              <p className="text-xl text-slate-600"> 03/20 - 06/20</p>
+            </div>
+            <p className="text-xl text-slate-400 tracking-wide"> Worked on building AR Solution for Retail theme with Chumbak as Theme Partner</p>
+          </div>
+
+
+        </div>
+
+
+      </div>
+
+      <div className="py-6 mx-40">
+        <br />
+        <hr className="h-px bg-slate-200 border-0 dark:bg-slate-700" />
+        <br />
+      </div>
+
+
+
+      <div className="flex flex-col justify-center items-center sm:gap-y-4 md:gap-y-8 lg:gap-y-16 md:mx-10 lg:mx-60 my-20">
         <div className="flex flex-col gap-y-4">
-          <h2 className="text-4xl text-center tracking-wide leading-6 lg:leading-12 lg:text-6xl bg-gradient-to-br from-slate-300 to-slate-500 inline-block text-transparent bg-clip-text lowercase">Here's What My friends & Colleagues told about me</h2>
+          <h2 className="text-3xl text-center tracking-wide leading-6 lg:leading-12 lg:text-5xl bg-gradient-to-br from-slate-300 to-slate-500 inline-block text-transparent bg-clip-text">Here's what my friends & colleagues say about me</h2>
 
           <InfiniteMovingCardsDemo />
 
