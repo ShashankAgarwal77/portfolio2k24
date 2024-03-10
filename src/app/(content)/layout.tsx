b9f9ab7a6998bdb9730106926eb15fb8ca7034c7
Shashank Agarwal <shashankagarwal.official@gmail.com>
2024-03-10 13:44:26 +0530

    meta data added

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -1,11 +1,18 @@
-import FooterComp from '../components/uiFrontend/footer'
-import { NavbarComp } from '../components/uiFrontend/navbar'
-import '../globals.css'
+import FooterComp from '../components/uiFrontend/footer';
+import { NavbarComp } from '../components/uiFrontend/navbar';
+import '../globals.css';
 // import { Header, Footer } from '@/components/Layout'
 // import 'react-toastify/dist/ReactToastify.css'
+
+import MetaDataThumbnail from '@/app/Assets/Images/MetaDataThumbnail.png';
+
+import Head from 'next/head';
+
 export const metadata = {
   description: 'A UX portfolio website of Shashank Agarwal. Designed & Developed by Shashank Agarwal using NextJs, Typescript and Framer Motion',
-  title: 'Shashank Agarwal - Product Designer',
+  title: 'Shashank Agarwal | Product Designer and Developer',
+  image: MetaDataThumbnail,
+
 }
 
 export default function ContentRootLayout({ children } : any) {
