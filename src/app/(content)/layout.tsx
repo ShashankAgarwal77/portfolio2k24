38ee7a61cfde2bc86b59da33d4ae0cc84a55804c
Shashank Agarwal <shashankagarwal.official@gmail.com>
2024-03-10 21:46:46 +0530

    meta image added

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -4,14 +4,11 @@ import '../globals.css';
 // import { Header, Footer } from '@/components/Layout'
 // import 'react-toastify/dist/ReactToastify.css'
 
-import MetaDataThumbnail from '@/app/Assets/Images/MetaDataThumbnail.png';
-
 import Head from 'next/head';
 
 export const metadata = {
   description: 'A UX portfolio website of Shashank Agarwal',
   title: 'Shashank Agarwal | Product Designer and Developer',
-  image: MetaDataThumbnail,
 
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
-import type { Metadata } from "next";
+import type { Metadata as NextMetadata } from "next";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
-import Head from 'next/head';
 
-// Import the PNG file
-const MetaDataThumbnail = require('@/app/Assets/Images/MetaDataThumbnail.png') as string;
-
+// Extend the Metadata type to include the image property
+interface Metadata extends NextMetadata {
+  image?: {
+    url: string;
+    alt: string;
+  };
+}
 
 const workSans = Work_Sans({ subsets: ["latin"] });
 
-// Define a custom type for metadata with image
-interface CustomMetadata extends Metadata {
-  image: string; // Assuming MetaDataThumbnail is a local path
-}
+// Define the relative path to your image
+const imagePath = "/Assets/Images/MetaDataThumbnail.png";
 
-// Define the metadata
-export const metadata: CustomMetadata = {
+export const metadata: Metadata = {
   title: "Shashank Agarwal | Product Designer and Developer",
-  description: "A UX portfolio website of Shashank Agarwal. Designed & Developed by Shashank Agarwal using NextJs, Typescript and Framer Motion",
-  // Provide the local path to the imported PNG file
-  image: MetaDataThumbnail as string, // Type assertion
+  description: "A UX portfolio website of Shashank Agarwal.",
+  // Add image metadata with the relative path
+  image: {
+    url: imagePath,
+    alt: "Description of your image",
+  },
 };
 
 export default function RootLayout({
