afbb74b38b69ba4c2c196f9b77331c4fffa8f259
Shashank Agarwal <shashankagarwal.official@gmail.com>
2024-03-28 14:52:15 +0530

    meta data added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
-import { Metadata } from "next";
+// import { Metadata } from "next";
+
+import Head from "next/head";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
 
 const workSans = Work_Sans({ subsets: ["latin"] });
 
 // Now, you can use the Metadata type for your metadata object
-export const metadata: Metadata = {
-  title: "Shashank Agarwal | Product Designer and Developer",
-  description: "A UX portfolio website of Shashank Agarwal.",
-};
+// export const metadata: Metadata = {
+//   title: "Shashank Agarwal | Product Designer and Developer",
+//   description: "A UX portfolio website of Shashank Agarwal.",
+// };
 
 
 export default function RootLayout({
@@ -18,9 +20,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="twitter:image" content="https://shashankagarwal.netlify.app/metaImage.jpg" />
-      </head>
+      <Head>
+        <title>Shashank Agarwal Digital Room</title>
+        <meta name="description" content="A UX portfolio website of a product designer Shashank Agarwal." />
+        <meta property="og:title" content="Shashank Agarwal | Product Designer and Developer" />
+        <meta property="og:description" content="A UX portfolio website of a product designer Shashank Agarwal." />
+        <meta property="og:image" content="https://shashankagarwal.netlify.app/metaImage.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://shashankagarwal.netlify.app/" />
+        {/* Add additional tags as needed */}
+      </Head>
 
       <body>{children}</body>
     </html>
