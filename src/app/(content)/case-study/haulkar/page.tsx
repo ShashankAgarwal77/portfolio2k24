fbec2f43261956d2e66e6c6c7e47628c4b5f5903
ShashankAgarwal77 <shashankagarwal.official@gmail.com>
2025-01-18 14:24:34 +0530

    client id of case studies has been comment out

--- src/app/(content)/case-study/haulkar/page.tsx
+++ src/app/(content)/case-study/haulkar/page.tsx
@@ -8,10 +8,12 @@ export default function HaulkarCaseStudy() {
         <div className="flex justify-center items-center h-screen p-10">
             <div className='w-full h-full rounded-md overflow-hidden '
                 dangerouslySetInnerHTML={{
-                    __html: `<iframe style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius:inherit;" width="100%" height="100%" src="https://embed.figma.com/deck/6p8Q1ccyFQpDu6KoR4XxTS/Simplifying-gig-work-in-logistics-presentation?node-id=1-618&node-type=canvas&viewport=-1876%2C-2574%2C0.54&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&embed-host=share&footer=false&theme=system&client-id=muds0emC09G4KReBWgDnIe" allowfullscreen></iframe>`,
+                    __html: `<iframe style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius:inherit;" width="100%" height="100%" src="https://embed.figma.com/deck/6p8Q1ccyFQpDu6KoR4XxTS/Simplifying-gig-work-in-logistics-presentation?node-id=1-618&node-type=canvas&viewport=-1876%2C-2574%2C0.54&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&embed-host=share&footer=false&theme=system" allowfullscreen></iframe>`,
                 }}
             />
         </div>
 
     );
 }
+
+ // client-id=muds0emC09G4KReBWgDnIe
--- src/app/(content)/case-study/oro/page.tsx
+++ src/app/(content)/case-study/oro/page.tsx
@@ -7,7 +7,7 @@ export default function HaulkarCaseStudy() {
         <div className="flex justify-center items-center h-screen p-10">
             <div className='w-full h-full rounded-md overflow-hidden '
                 dangerouslySetInnerHTML={{
-                    __html: `<iframe style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius:inherit;" width="100%" height="100%" src="https://embed.figma.com/proto/pfd6MQguGv2xmtPQ28XJoC/%F0%9F%92%BB-Portfolio-Presentation?page-id=317%3A157&node-id=328-3481&node-type=frame&viewport=1257%2C481%2C0.39&scaling=contain&content-scaling=fixed&embed-host=share&footer=false&theme=system&client-id=muds0emC09G4KReBWgDnIe" allowfullscreen></iframe>`,
+                    __html: `<iframe style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius:inherit;" width="100%" height="100%" src="https://embed.figma.com/proto/pfd6MQguGv2xmtPQ28XJoC/%F0%9F%92%BB-Portfolio-Presentation?page-id=317%3A157&node-id=328-3481&node-type=frame&viewport=1257%2C481%2C0.39&scaling=contain&content-scaling=fixed&embed-host=share&footer=false&theme=system" allowfullscreen></iframe>`,
                 }}
             />
         </div>
